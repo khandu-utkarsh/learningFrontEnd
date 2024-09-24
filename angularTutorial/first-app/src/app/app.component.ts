74162a9f2b0b52b596033567afb1ded1e5f27c90
utkarsh <utkarshkhandelwal2011@gmail.com>
2024-09-24 19:50:54 -0400

    Routing working

--- angularTutorial/first-app/src/app/app.component.ts
+++ angularTutorial/first-app/src/app/app.component.ts
@@ -1,22 +1,22 @@
 import {Component} from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import {RouterModule} from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root', //!This is CSS selector
   standalone: true, //!Independent component, not a module --> What does this exactly mean? 
-  imports: [HomeComponent, RouterModule],
+  imports: [HomeComponent, RouterLink, RouterOutlet],
   template: `
-  <main>
-    <a [routerLink]="['/']">
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
-    </a>
-    <section class="content">
-      <router-outlet></router-outlet>
-    </section>
-  </main>
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
   `,
   styleUrls: ['./app.component.css'],
 })
