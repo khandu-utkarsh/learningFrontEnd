b8ee804738bb0d9aa2fe0bebce539af67769bd27
utkarsh <utkarshkhandelwal2011@gmail.com>
2024-09-24 21:07:34 -0400

    Filtering and all working

--- angularTutorial/first-app/src/app/home/home.component.ts
+++ angularTutorial/first-app/src/app/home/home.component.ts
@@ -10,29 +10,37 @@ import {HousingService} from '../housing.service';
   template: `
   <section>
   <form>
-  <input type="text" placeholder="Filter by city" />
-  <button class="primary" type="button"> Search </button>
+  <input type="text" placeholder="Filter by city" #filter />
+  <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
   </form>
   </section>
   <section class="results">
-      <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
-        [housingLocation]="housingLocation"
-      ></app-housing-location>
-    </section>
-
-
-
+    <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
+  </section>
   `,
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
 
+  filteredLocationList: HousingLocation[] = [];
+
   housingService: HousingService = inject(HousingService);
   housingLocationList: HousingLocation[] = [ ]; //!Do I need to define it, or can it be like javascript and declared on the go?
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
 
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      //console.log('Hit this and should not have')
+      return;
+    }
+    //console.log(`Input text sent to us: ${text}`);    
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
 
+    );
+  }
 }
