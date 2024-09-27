0604dd0a9235ddaacd6b6c6b11e2cb9f2a75c3a5
utkarsh <utkarshkhandelwal2011@gmail.com>
2024-09-26 22:10:30 -0400

    Forms not working

--- angularTutorial/myWeatherApp/src/app/app.component.ts
+++ angularTutorial/myWeatherApp/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OpenmeteoWeatherServiceService } from './openmeteo-weather-service.service';
+import { NgModule } from '@angular/core';
+
+//!Let's put everything in the root and create a button.
+
 
 @Component({
   selector: 'app-root',
@@ -11,18 +15,37 @@ import { OpenmeteoWeatherServiceService } from './openmeteo-weather-service.serv
 })
 export class AppComponent {
   title = 'myWeatherApp';
-  params = {
-    "latitude": 52.52,
-    "longitude": 13.41,
-    "hourly": "temperature_2m"
-  };
+  latitude: string = '';
+  longitude: string = '';
 
   private ws = inject(OpenmeteoWeatherServiceService);
   constructor() {
-    this.ws.getWeatherInfoForParams(this.params);
+
   }
 
 
+  submitButtonClicked() {
+
+
+    let params = {
+      "latitude": this.latitude,
+      "longitude": this.longitude,
+      "hourly": "temperature_2m"
+    };
+  
+    console.log(this.latitude);
+    console.log(this.longitude);
+
+    if (this.latitude && this.longitude) {
+
+      //!This got called.
+      this.ws.getWeatherInfoForParams(params); //!It is printing on the console inside it.
+      console.log('Coordinates:', {        latitude: this.latitude,        longitude: this.longitude});
+    }
+    else {
+      console.error('Please provide both latitude and longitude.');
+    }
+  }
 
 
   //!Let's inject that service directly in this root component.
