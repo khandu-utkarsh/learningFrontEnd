2f4cb288e95a3a83428a46352fbfd065ff5b87c9
utkarsh <utkarshkhandelwal2011@gmail.com>
2024-12-10 23:38:04 -0500

    Check point for constructing the burger menu

--- next-blog/.next/static/chunks/webpack.js
+++ next-blog/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6344537fed746fe8"; }
+/******/ 		__webpack_require__.h = function() { return "b45be07474f2b163"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next-blog/src/components/authLinks/AuthLinks.jsx
+++ next-blog/src/components/authLinks/AuthLinks.jsx
@@ -1,20 +1,41 @@
+"use client"
+
 import Link from "next/link";
 import styles from "./authLinks.module.css"
+import { useState } from "react";
 
 const AuthLinks = () => {
 
+    const [open, setOpen] = useState(false)
+
+
     const status = "notAuthenticated"   //!Temp
 
-    const loginLink = <Link href="/login">Login</Link>
+    const loginLink = <Link href="/login" className={styles.link}>Login</Link>
     const logoutLink = 
         <>
-            <Link href="/write">Write</Link>
+            <Link href="/write" className={styles.link}>Write</Link>
             <span className={styles.link}>Logout</span>
         </>
+    const burgerMenu =
+        <div className={styles.burger} onClick={() => setOpen(!open)}>
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>    
+        </div>
 
+    const burgerMenuOpen = 
+        <>
+            <Link href="/" >Homepage</Link>
+            <Link href="/" >Contact</Link>
+            <Link href="/" >About</Link>
+            {status === "notAuthenticated" ? loginLink :logoutLink }
+        </>
     return (
         <>
             {status === "notAuthenticated" ? loginLink :logoutLink }
+            {burgerMenu}
+            {open && burgerMenuOpen}
         </>
     );
 };
