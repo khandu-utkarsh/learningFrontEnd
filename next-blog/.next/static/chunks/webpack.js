c4b4e91cd6cb284fa5ab94c6a1bed80207dc7a5f
utkarsh <utkarshkhandelwal2011@gmail.com>
2024-12-11 23:29:39 -0500

    Added some media query and Done mostly with home page.

--- next-blog/.next/static/chunks/webpack.js
+++ next-blog/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1c25127233ec6e07"; }
+/******/ 		__webpack_require__.h = function() { return "3b380f8327e51a07"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
