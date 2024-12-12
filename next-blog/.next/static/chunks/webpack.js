587d0b097a7084f473656097790e9a3ad1db11b4
utkarsh <utkarshkhandelwal2011@gmail.com>
2024-12-11 23:51:51 -0500

    Added few routes, also added dynamic route. Have to read about them

--- next-blog/.next/static/chunks/webpack.js
+++ next-blog/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3b380f8327e51a07"; }
+/******/ 		__webpack_require__.h = function() { return "ab60012d50988f97"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ next-blog/src/app/[slug]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./singlePage.module.css"
+
+const SinglePage = () => {
+    return (
+        <div className={styles.container}>Single Page</div>
+    )
+
+}
+
+export default SinglePage
