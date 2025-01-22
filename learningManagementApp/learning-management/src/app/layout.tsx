0d5fe0f104959d816954a3130b1b3671f1e16bd4
utkarsh <utkarshkhandelwal2011@gmail.com>
2025-01-21 22:33:11 -0500

    Checkpointing the redux page.

--- learningManagementApp/learning-management/src/app/layout.tsx
+++ learningManagementApp/learning-management/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -23,7 +24,9 @@ export default function RootLayout({
       <body
         className={`${dmSans.className}`}
       >
+        <Providers>
         {children}
+        </Providers>
       </body>
     </html>
   );
