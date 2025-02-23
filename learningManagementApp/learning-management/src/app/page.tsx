5835e4176aa233e2e32ffa6ce7ef1571fffb1dd0
utkarsh <utkarshkhandelwal2011@gmail.com>
2025-02-23 14:24:33 -0500

    Checkpointing carousel progress.

--- learningManagementApp/learning-management/src/app/page.tsx
+++ learningManagementApp/learning-management/src/app/page.tsx
@@ -1,9 +1,14 @@
-import NonDashboardNavbar from "@/components/NonDashboardNavbar"
+import NonDashboardNavbar from "@/components/NonDashboardNavbar";
+import Landing from "@/app/(nondashboard)/landing/page";
+
 
 export default function Home() {
   return (
     <div className="nondashboard-layout">
       <NonDashboardNavbar />
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
     </div>
   );
 }
