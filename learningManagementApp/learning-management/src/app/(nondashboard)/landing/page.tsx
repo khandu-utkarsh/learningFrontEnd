754b716677e5b9facf08f40908607b6769c347d4
utkarsh <utkarshkhandelwal2011@gmail.com>
2025-02-25 23:23:51 -0500

    Updating the landing page with the server side information.

--- learningManagementApp/learning-management/src/app/(nondashboard)/landing/page.tsx
+++ learningManagementApp/learning-management/src/app/(nondashboard)/landing/page.tsx
@@ -6,6 +6,9 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { useCarousel } from '@/hooks/useCarousel'
 import { Skeleton } from '@/components/ui/skeleton'
+import { useGetCoursesQuery } from '@/state/api'
+import CourseCardSearch from "@/components/CourseCardSearch"
+import { useRouter } from 'next/navigation'
 
 
 const LoadingSkeleton = () => {
@@ -40,9 +43,17 @@ const LoadingSkeleton = () => {
 }
 
 function Landing() {
-
+    const router = useRouter()
     const currentImage = useCarousel({totalImages: 3})
     console.log('Value of currImage is: ', currentImage)
+
+    const {data: courses, isLoading, isError} = useGetCoursesQuery({})
+ 
+    const handleCourseClick = (courseId: string) => {
+        router.push(`/search?id=${courseId}`)
+    }
+ 
+    console.log("courses from api are:  ", courses)
 
   return (
     <motion.div 
@@ -108,6 +119,18 @@ function Landing() {
         </div>
 
         <div className='landing__courses'>
+            {courses && 
+            courses.slice(0,4).map((course, index) => (
+                <motion.div
+                key= {course.courseId}   
+                initial={{y: 50, opacity: 0}}
+                whileInView={{y : 0, opacity: 1}}
+                transition={{duration: 0.5, delay: index * 0.2}}        
+                viewport={{amount: 0.4}}
+            >
+                <CourseCardSearch course= {course} onClick={() => handleCourseClick(course.courseId)}></CourseCardSearch>
+                </motion.div>
+            ))}
             {/* COURSES DISPLAY */}
         </div>
     </motion.div>
--- learningManagementApp/learning-management/src/state/api.ts
+++ learningManagementApp/learning-management/src/state/api.ts
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BaseQueryApi, createApi, FetchArgs, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import exp from "constants";
 import build from "next/dist/build";
 
@@ -251,12 +251,49 @@ import build from "next/dist/build";
 //   useUpdateUserCourseProgressMutation,
 // } = api;
 
+const customBaseQuery = async (
+  args: string | FetchArgs,
+  api: BaseQueryApi,
+  extraOptions: {}
+) => {
+  const baseQuery = fetchBaseQuery({baseUrl : process.env.NEXT_PUBLIC_API_BASE_URL});
 
-export const api = createApi({
-  baseQuery : fetchBaseQuery({baseUrl : process.env.NEXT_PUBLIC_API_BASE_URL}),
-  reducerPath: "api",
-  tagTypes: [],
-  endpoints: (build) => ({}),
+  try {
+    const result : any = await baseQuery(args, api, extraOptions);
+
+    if(result.data) {
+      result.data = result.data.data;
+    }
+    return result;
+  } catch(error : unknown) {
+    const errorMessage = error instanceof Error ? error.message : "Unkonwn error";
+
+    return {error: {status: "FETCH_ERROR", error : errorMessage}};
+  }
+};
+
+export const api = createApi(
+  {
+    baseQuery : customBaseQuery,
+    reducerPath: "api",
+    tagTypes: ["Courses"],
+
+    endpoints: (build) => (
+      {
+        getCourses: build.query<Course[], {category?: string}>(
+          {            
+            query: ({category}) =>({ url: "courses", params: {category} }),
+            providesTags: ["Courses"]
+          }),
+        getCourse: build.query<Course, string>(
+          {
+            query: (id) => `courses/${id}`,
+            providesTags: (result, error, id) => [{type: "Courses", id}]
+          })
+      }),
 });
 
-export const {} = api;
+export const {
+  useGetCoursesQuery,
+  useGetCourseQuery
+} = api;
